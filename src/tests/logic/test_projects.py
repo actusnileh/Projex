a0daaeccee3b953679eb815444dcdd07e18396c3
actusnileh <actusnileh@gmail.com>
2024-11-10 18:43:09 +0300

    fead: added faker for tests

--- src/tests/logic/test_projects.py
+++ src/tests/logic/test_projects.py
@@ -1,5 +1,6 @@
 import pytest
 from domain.entities.projects import Project
+from faker import Faker
 from infrastructure.repositories.projects import BaseProjectRepository
 from logic.commands.projects import CreateProjectCommand
 from logic.mediator import Mediator
@@ -10,9 +11,11 @@ async def test_create_project_command_success(
     project_repository: BaseProjectRepository,
     mediator: Mediator,
 ):
-    # TODO: Сделать faker для генерации случайных текстов
+    faker = Faker()
+    random_title = faker.text(max_nb_chars=20)
+
     project: Project = (
-        await mediator.handle_command(CreateProjectCommand(title="Project 1"))
+        await mediator.handle_command(CreateProjectCommand(title=random_title))
     )[0]
 
     assert project_repository.check_project_exists_by_title(
